6e2391ab9f1c5ed2f5edfb2abdf70816d93813f4
Christian Voigt <wwevo@users.noreply.github.com>
2018-01-12 03:02:31 +0100

    enhanced Location class a bit, put logic for boundaries inside and out of the action files to keep em simple. once the bot is done, other people can write actions and observers, this should be as easy as possible

--- actions_lobby.py
+++ actions_lobby.py
@@ -97,46 +97,57 @@ here come the observers
 observers_lobby = []
 
 
-def player_left_area(self, player_object, locations):
+def player_is_outside_boundary(self, player_object, locations):
     try:
         location = locations["lobby"]
     except KeyError:
         return False
 
     if not player_object.authenticated and player_object.is_responsive:
-        distance_to_lobby_center = float(math.sqrt(
-                (float(location.pos_x) - float(player_object.pos_x)) ** 2 + (
-                    float(location.pos_y) - float(player_object.pos_y)) ** 2 + (
-                    float(location.pos_z) - float(player_object.pos_z)) ** 2))
-
-        if distance_to_lobby_center > location.radius:
+        if location.player_is_outside_boundary(player_object):
             if self.tn.teleportplayer(player_object, location):
                 self.tn.say("You have been ported to the lobby! Authenticate with /password <password>")
             # time.sleep(2)  # possibly not the best way to avoid mutliple teleports in a row
 
 
-observers_lobby.append(("player left lobby", player_left_area, "(self, player_object, locations)"))
+observers_lobby.append(("player left lobby", player_is_outside_boundary, "(self, player_object, locations)"))
 
 
-def player_approaching_boundary_from_inside(self, player_object, locations):
+def player_is_near_boundary_inside(self, player_object, locations):
     try:
         location = locations["lobby"]
     except KeyError:
         return False
 
     if not player_object.authenticated:
-        distance_to_lobby_center=float(math.sqrt(
-            (float(location.pos_x) - float(player_object.pos_x)) ** 2 + (
-                    float(location.pos_y) - float(player_object.pos_y)) ** 2 + (
-                    float(location.pos_z) - float(player_object.pos_z)) ** 2))
-
-        if distance_to_lobby_center >= (location.radius * 0.75) and distance_to_lobby_center <= location.radius:
+        if location.player_is_near_boundary_inside(player_object):
             # TODO: this needs to be a sheduled message on a set timer. like every second, every two seconds.
             #       we will also need that to remind people if they are in a reset zone or otherwise noteworthy location
             #       player is in location -> start message
             #       player is in boundary -> increase frequency and or clolor?
-            #       okayer left location -> end message
+            #       player left location -> end message
+            #           thought about it... locations should have their own triggers which can be polled here
+            #           so it could look like
+            #               if player_object is location_object.near:
+            #               if player_object is location_object.entering:
+            #               if player_object is location_object.inside_boundary:
+            #               if player_object is location_object.inside_core:
+            #           all regions affected by the location should be stored and then compared to the players current
+            #           region
             self.tn.say("get your behind back in the lobby or we'll manhandle you there rudely!")
 
 
-observers_lobby.append(("player approaching boundary from inside", player_approaching_boundary_from_inside, "(self, player_object ,locations)"))
+observers_lobby.append(("player approaching boundary from inside", player_is_near_boundary_inside, "(self, player_object ,locations)"))
+
+
+def player_is_near_boundary_outside(self, player_object, locations):
+    try:
+        location = locations["lobby"]
+    except KeyError:
+        return False
+
+    if location.player_is_near_boundary_outside(player_object):
+        self.tn.say("you are near the lobby, there might be triggered actions. beware!")
+
+
+observers_lobby.append(("player approaching boundary from outside", player_is_near_boundary_outside, "(self, player_object ,locations)"))
--- location.py
+++ location.py
@@ -1,3 +1,6 @@
+import math
+
+
 class Location(object):
     owner = str
     name = str
@@ -6,6 +9,7 @@ class Location(object):
     pos_z = float
     shape = str
     radius = float
+    # Todo: region should be a list as a location nd it's effect can spawn several regions. capture all regions if empty
     region = str
 
     def __init__(self, **kwargs):
@@ -16,3 +20,40 @@ class Location(object):
     def update(self, **kwargs):
         for (k, v) in kwargs.iteritems():
             setattr(self, k, v)
+
+    def player_is_outside_boundary(self, player_object):
+        if self.shape == "sphere":
+            distance_to_lobby_center = float(math.sqrt(
+                (float(self.pos_x) - float(player_object.pos_x)) ** 2 + (
+                    float(self.pos_y) - float(player_object.pos_y)) ** 2 + (
+                    float(self.pos_z) - float(player_object.pos_z)) ** 2))
+
+        if distance_to_lobby_center > self.radius:
+            return True
+        else:
+            return False
+
+    def player_is_near_boundary_inside(self, player_object):
+        if self.shape == "sphere":
+            distance_to_lobby_center = float(math.sqrt(
+                (float(self.pos_x) - float(player_object.pos_x)) ** 2 + (
+                        float(self.pos_y) - float(player_object.pos_y)) ** 2 + (
+                        float(self.pos_z) - float(player_object.pos_z)) ** 2))
+
+        if distance_to_lobby_center >= (self.radius * 0.5) and distance_to_lobby_center <= self.radius:
+            return True
+        else:
+            return False
+
+    def player_is_near_boundary_outside(self, player_object):
+        if self.shape == "sphere":
+            distance_to_lobby_center = float(math.sqrt(
+                (float(self.pos_x) - float(player_object.pos_x)) ** 2 + (
+                        float(self.pos_y) - float(player_object.pos_y)) ** 2 + (
+                        float(self.pos_z) - float(player_object.pos_z)) ** 2))
+
+        if distance_to_lobby_center >= self.radius and distance_to_lobby_center <= (self.radius * 1.5):
+            return True
+        else:
+            return False
+
