6af55198f2dc5362335deffcffecbe1b5d4fdb01
Christian Voigt <wwevo@users.noreply.github.com>
2018-04-26 10:23:43 +0200

    small changes for an easier to follow install routine

--- chrani-bot.sh
+++ chrani-bot.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-pgrep -f chrani-bot.py || nohup python /opt/7dtd-chrani-bot/chrani-bot.py bot_testing --verbosity=DEBUG &
+pgrep -f chrani-bot.py || nohup python /opt/7dtd-chrani-bot/chrani-bot.py chrani-bot --verbosity=DEBUG &
