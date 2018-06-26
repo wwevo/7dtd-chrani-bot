92a6128c343c2b838c7e456a2b3f5b243e4202af
Christian Voigt <wwevo@users.noreply.github.com>
2018-06-26 18:46:05 +0200

    added /summon player command

--- bot/actions_players.py
+++ bot/actions_players.py
@@ -48,3 +48,49 @@ actions_players.append({
     "group": "players",
     "essential" : False
 })
+
+
+def teleport_player_to_self(self, command):
+    try:
+        player_object = self.bot.players.get(self.player_steamid)
+        p = re.search(r"summon\splayer\s(?P<steamid>([0-9]{17}))|(?P<entityid>[0-9]+)", command)
+        if p:
+            steamid_to_fetch = p.group("steamid")
+            entityid_to_fetch = p.group("entityid")
+            if steamid_to_fetch is None:
+                steamid_to_fetch = self.bot.players.entityid_to_steamid(entityid_to_fetch)
+                if steamid_to_fetch is False:
+                    self.tn.send_message_to_player(player_object, "could not find player", color=self.bot.chat_colors['error'])
+                    return False
+            if int(steamid_to_fetch) == int(player_object.steamid):
+                self.tn.send_message_to_player(player_object, "Hands off those drugs man. They ain't good for you!", color=self.bot.chat_colors['warning'])
+                return False
+            else:
+                player_object_to_teleport_to = self.bot.players.get(steamid_to_fetch)
+        else:
+            self.tn.send_message_to_player(player_object, "you did not specify a player. Use {}".format(self.bot.find_action_help("players", "summon player")), color=self.bot.chat_colors['warning'])
+            return False
+
+    except Exception as e:
+        logger.exception(e)
+        raise KeyError
+
+    coord_tuple = (player_object_to_teleport_to.pos_x, player_object_to_teleport_to.pos_y, player_object_to_teleport_to.pos_z)
+    if self.tn.teleportplayer(player_object, coord_tuple=coord_tuple):
+        self.tn.send_message_to_player(player_object, "You have been summoned to {}'s location".format(player_object_to_teleport_to.name), color=self.bot.chat_colors['success'])
+    else:
+        self.tn.send_message_to_player(player_object, "Summoning player {} failed :(".format(player_object_to_teleport_to.name), color=self.bot.chat_colors['error'])
+    return True
+
+
+actions_players.append({
+    "match_mode" : "startswith",
+    "command" : {
+        "trigger" : "summon player",
+        "usage" : "/summon player <steamid/entityid>"
+    },
+    "action" : teleport_player_to_self,
+    "env": "(self, command)",
+    "group": "players",
+    "essential" : False
+})
