85b5a751dc3ddd24cc00fdea1febe93d3beb2619
Christian Voigt <wwevo@users.noreply.github.com>
2018-07-02 20:34:32 +0200

    version bumped to 0.3c

--- chrani-bot.py
+++ chrani-bot.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     while True:
         try:
             bot = ChraniBot()
-            bot.bot_version = "0.3b"
+            bot.bot_version = "0.3c"
             bot.run()
         except (IOError, NameError) as error:
             """ clean up bot to have a clean restart when a new connection can be established """
