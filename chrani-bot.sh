9e80ac2e4c02a26bdf94ae37214fcb46df5a4e79
Christian Voigt <wwevo@users.noreply.github.com>
2018-08-12 15:31:54 +0200

    updated startscript for virtualenv bumped tp 0.4i

--- chrani-bot.sh
+++ chrani-bot.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+source /opt/7dtd-chrani-bot/venv_app/chrani-bot/bin/activate
 pgrep -f "chrani-bot.py chrani-bot" || nohup python /opt/7dtd-chrani-bot/app/chrani-bot.py chrani-bot --verbosity=DEBUG &
