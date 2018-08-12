e3f3ff3b4677fb17dc039dd2482e942ecc9103e0
Christian Voigt <wwevo@users.noreply.github.com>
2018-08-12 15:41:05 +0200

    fixed startscript for virtualenv usage

--- chrani-bot.sh
+++ chrani-bot.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-source /opt/7dtd-chrani-bot/venv_app/chrani-bot/bin/activate
 pgrep -f "chrani-bot.py chrani-bot" || . /opt/7dtd-chrani-bot/venv_app/chrani-bot/bin/activate && nohup python /opt/7dtd-chrani-bot/app/chrani-bot.py chrani-bot --verbosity=DEBUG &
