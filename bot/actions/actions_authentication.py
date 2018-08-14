adc9f15d12323af4342b45caa4532d752a0446da
Christian Voigt <wwevo@users.noreply.github.com>
2018-08-14 23:17:31 +0200

    cleaned up authentication actions a bit

--- bot/actions/actions_authentication.py
+++ bot/actions/actions_authentication.py
@@ -1,5 +1,4 @@
 import re
-from bot.modules.logger import logger
 import common
 from bot.assorted_functions import ResponseMessage
 
@@ -13,7 +12,8 @@ def on_enter_gameworld(bot, source_player, target_player, command):
     notes:
     does nothing for authenticated players
     """
-    if target_player.has_permission_level("authenticated") is not True:
+    response_messages = ResponseMessage()
+    if not target_player.has_permission_level("authenticated"):
         bot.tn.send_message_to_player(target_player, "read the rules on https://chrani.net/chrani-bot", color=bot.chat_colors['warning'])
         bot.tn.send_message_to_player(target_player, "this is a development server. you can play here, but there's no support or anything really.", color=bot.chat_colors['info'])
         bot.tn.send_message_to_player(target_player, "Enjoy!", color=bot.chat_colors['info'])
@@ -21,8 +21,9 @@ def on_enter_gameworld(bot, source_player, target_player, command):
     if target_player.initialized is not True:
         target_player.initialized = True
         bot.players.upsert(target_player)
+        response_messages.add_message("Player {} is now initialized".format(target_player.name), True)
 
-    return ResponseMessage()
+    return response_messages
 
 
 common.actions_list.append({
@@ -32,7 +33,6 @@ common.actions_list.append({
         "usage": None
     },
     "action": on_enter_gameworld,
-    "env": "(self)",
     "group": "authentication",
     "essential": True
 })
@@ -45,7 +45,6 @@ common.actions_list.append({
         "usage": None
     },
     "action": on_enter_gameworld,
-    "env": "(self)",
     "group": "authentication",
     "essential": True
 })
@@ -53,6 +52,7 @@ common.actions_list.append({
 
 def password(bot, source_player, target_player, command):
     """Adds player to permission-group(s) according to the password entered
+    will remove authenticated status if the player enters a woing password
 
     Keyword arguments:
     self -- the bot
@@ -67,43 +67,61 @@ def password(bot, source_player, target_player, command):
     notes:
     the password must exist in the password dictionary
     """
-    p = re.search(r"password\s(?P<password>\w+)$", command)
-    try:
+    p = re.search(r"^password\s?(?P<password>\w+)$|^password\s?$", command)
+    if p:
+        response_messages = ResponseMessage()
         pwd = p.group("password")
-    except (AttributeError, IndexError) as e:
-        bot.tn.send_message_to_player(target_player, "You have entered no password. Use {}".format(common.find_action_help("authentication", "password")), color=bot.chat_colors['warning'])
-        return False
+        if not pwd:
+            message = "No password provided"
+            bot.tn.send_message_to_player(target_player, "You have entered no password. Use {}".format(common.find_action_help("authentication", "password")), color=bot.chat_colors['warning'])
+            response_messages.add_message(message, False)
+        elif pwd not in bot.passwords.values() and not target_player.authenticated:
+            message = "Entered a wrong / unknown password"
+            bot.tn.send_message_to_player(target_player, "You have entered a wrong password!", color=bot.chat_colors['warning'])
+            response_messages.add_message(message, False)
+        elif pwd not in bot.passwords.values() and target_player.authenticated:
+            target_player.set_authenticated(False)
+            message = "Entered a wrong / unknown password"
+            response_messages.add_message(message, False)
+            target_player.remove_permission_level("authenticated")
+            message = "You have lost your authentication!"
+            response_messages.add_message(message, False)
+            bot.tn.send_message_to_player(target_player, message, color=bot.chat_colors['warning'])
+            bot.players.upsert(target_player, save=True)
+            bot.webinterface.socketio.emit('update_player_table_row', {"steamid": target_player.steamid, "entityid": target_player.entityid}, namespace='/test')
+        elif pwd in bot.passwords.values():
+            if not target_player.authenticated:
+                message = "{} joined the ranks of literate people. Welcome!".format(target_player.name)
+                response_messages.add_message(message, True)
+                bot.tn.say(message, color=bot.chat_colors['background'])
 
-    if pwd not in bot.passwords.values() and target_player.authenticated is not True :
-        bot.tn.send_message_to_player(target_player, "You have entered a wrong password!", color=bot.chat_colors['warning'])
-        return False
-    elif pwd not in bot.passwords.values() and target_player.authenticated is True:
-        target_player.set_authenticated(False)
-        target_player.remove_permission_level("authenticated")
-        bot.tn.send_message_to_player(target_player, "You have lost your authentication!", color=bot.chat_colors['warning'])
+            target_player.add_permission_level("authenticated")
+            message = "You were added to the group authenticated"
+            response_messages.add_message(message, True)
 
-        bot.players.upsert(target_player, save=True)
-        return False
+            """ makeshift promotion system """
+            # TODO: well, this action should only care about general authentication. things like admin and mod should be handled somewhere else really
+            if pwd == bot.passwords['admin']:
+                target_player.add_permission_level("admin")
+                message = "you are an Admin"
+                response_messages.add_message(message, True)
+                bot.tn.send_message_to_player(target_player, message, color=bot.chat_colors['success'])
+            elif pwd == bot.passwords['mod']:
+                target_player.add_permission_level("mod")
+                message = "you are a Moderator"
+                response_messages.add_message(message, True)
+                bot.tn.send_message_to_player(target_player, message, color=bot.chat_colors['success'])
+            elif pwd == bot.passwords['donator']:
+                target_player.add_permission_level("donator")
+                message = "you are a Donator. Thank you <3"
+                response_messages.add_message(message, True)
+                bot.tn.send_message_to_player(target_player, message, color=bot.chat_colors['success'])
 
-    if target_player.authenticated is not True:
-        target_player.set_authenticated(True)
-        target_player.add_permission_level("authenticated")
-        bot.tn.say("{} joined the ranks of literate people. Welcome!".format(target_player.name), color=bot.chat_colors['background'])
-
-    """ makeshift promotion system """
-    # TODO: well, this action should only care about general authentication. things like admin and mod should be handled somewhere else really
-    if pwd == bot.passwords['admin']:
-        target_player.add_permission_level("admin")
-        bot.tn.send_message_to_player(target_player, "you are an Admin", color=bot.chat_colors['success'])
-    elif pwd == bot.passwords['mod']:
-        target_player.add_permission_level("mod")
-        bot.tn.send_message_to_player(target_player, "you are a Moderator", color=bot.chat_colors['success'])
-    elif pwd == bot.passwords['donator']:
-        target_player.add_permission_level("donator")
-        bot.tn.send_message_to_player(target_player, "you are a Donator. Thank you <3", color=bot.chat_colors['success'])
-
-    bot.players.upsert(target_player, save=True)
-    return True
+            bot.webinterface.socketio.emit('update_player_table_row', {"steamid": target_player.steamid, "entityid": target_player.entityid}, namespace='/test')
+            bot.players.upsert(target_player, save=True)
+        return response_messages
+    else:
+        raise ValueError("action does not fully match the trigger-string")
 
 
 common.actions_list.append({
@@ -113,7 +131,6 @@ common.actions_list.append({
         "usage": "/password <password>"
     },
     "action": password,
-    "env": "(self, command)",
     "group": "authentication",
     "essential": True
 })
@@ -137,36 +154,45 @@ def add_player_to_permission_group(bot, source_player, target_player, command):
     """
     p = re.search(r"add\splayer\s((?P<steamid>([0-9]{17}))|(?P<entityid>[0-9]+))\sto\sgroup\s(?P<group_name>\w+)", command)
     if p:
+        response_messages = ResponseMessage()
         steamid_to_modify = p.group("steamid")
         entityid_to_modify = p.group("entityid")
         if steamid_to_modify is None:
             steamid_to_modify = bot.players.entityid_to_steamid(entityid_to_modify)
-            if steamid_to_modify is False:
-                raise KeyError
 
-        response_messages = ResponseMessage()
+        player_exists = False
+        try:
+            player_object_to_modify = bot.players.get_by_steamid(steamid_to_modify)
+            player_exists = True
+        except Exception as e:
+            if steamid_to_modify is False:
+                message = "Could not find a player to match entityid {}".format(entityid_to_modify)
+                response_messages.add_message(message, False)
+            else:
+                message = "could not find a player to match steamid {}".format(steamid_to_modify)
+                response_messages.add_message(message, False)
+            bot.tn.send_message_to_player(target_player, message, color=bot.chat_colors['warning'])
+
+        group_exists = False
         group = str(p.group("group_name"))
-        if group not in bot.permission_levels_list:
+        if group in bot.permission_levels_list:
+            group_exists = True
+        else:
             message = "the group {} does not exist!".format(group)
             response_messages.add_message(message, False)
             bot.tn.send_message_to_player(target_player, message, color=bot.chat_colors['warning'])
 
-        try:
-            player_object_to_modify = bot.players.get_by_steamid(steamid_to_modify)
+        if player_exists and group_exists:
             player_object_to_modify.add_permission_level(group)
             message = "{} has been added to the group {}".format(player_object_to_modify.name, group)
             response_messages.add_message(message, True)
             bot.tn.send_message_to_player(target_player, message, color=bot.chat_colors['success'])
-
-            bot.webinterface.socketio.emit('refresh_permissions', {"steamid": player_object_to_modify.steamid, "entityid": player_object_to_modify.entityid}, namespace='/test')
-
+            bot.webinterface.socketio.emit('update_player_table_row', {"steamid": player_object_to_modify.steamid, "entityid": player_object_to_modify.entityid}, namespace='/test')
             bot.players.upsert(player_object_to_modify, save=True)
-        except Exception:
-            message = "could not find a player with steamid {}".format(steamid_to_modify)
-            response_messages.add_message(message, False)
-            bot.tn.send_message_to_player(target_player, message, color=bot.chat_colors['warning'])
 
         return response_messages
+    else:
+        raise ValueError("action does not fully match the trigger-string")
 
 
 common.actions_list.append({
@@ -176,7 +202,6 @@ common.actions_list.append({
         "usage": "/add player <steamid/entityid> to group <group name>"
     },
     "action": add_player_to_permission_group,
-    "env": "(self, command)",
     "group": "authentication",
     "essential": False
 })
@@ -200,36 +225,45 @@ def remove_player_from_permission_group(bot, source_player, target_player, comma
     """
     p = re.search(r"remove\splayer\s((?P<steamid>([0-9]{17}))|(?P<entityid>[0-9]+))\sfrom\sgroup\s(?P<group_name>\w+)", command)
     if p:
+        response_messages = ResponseMessage()
         steamid_to_modify = p.group("steamid")
         entityid_to_modify = p.group("entityid")
         if steamid_to_modify is None:
             steamid_to_modify = bot.players.entityid_to_steamid(entityid_to_modify)
-            if steamid_to_modify is False:
-                raise KeyError
 
-        response_messages = ResponseMessage()
+        player_exists = False
+        try:
+            player_object_to_modify = bot.players.get_by_steamid(steamid_to_modify)
+            player_exists = True
+        except Exception as e:
+            if steamid_to_modify is False:
+                message = "Could not find a player to match entityid {}!".format(entityid_to_modify)
+                response_messages.add_message(message, False)
+            else:
+                message = "could not find a player with steamid {}".format(steamid_to_modify)
+                response_messages.add_message(message, False)
+            bot.tn.send_message_to_player(target_player, message, color=bot.chat_colors['warning'])
+
+        group_exists = False
         group = str(p.group("group_name"))
-        if group not in bot.permission_levels_list:
+        if group in bot.permission_levels_list:
+            group_exists = True
+        else:
             message = "the group {} does not exist!".format(group)
             response_messages.add_message(message, False)
             bot.tn.send_message_to_player(target_player, message, color=bot.chat_colors['warning'])
 
-        try:
-            player_object_to_modify = bot.players.get_by_steamid(steamid_to_modify)
+        if player_exists and group_exists:
             player_object_to_modify.remove_permission_level(group)
             message = "{} has been removed from the group {}".format(player_object_to_modify.name, group)
             response_messages.add_message(message, True)
             bot.tn.send_message_to_player(target_player, message, color=bot.chat_colors['success'])
-
-            bot.webinterface.socketio.emit('refresh_permissions', {"steamid": player_object_to_modify.steamid, "entityid": player_object_to_modify.entityid}, namespace='/test')
-
+            bot.webinterface.socketio.emit('update_player_table_row', {"steamid": player_object_to_modify.steamid, "entityid": player_object_to_modify.entityid}, namespace='/test')
             bot.players.upsert(player_object_to_modify, save=True)
-        except Exception:
-            message = "could not find a player with steamid {}".format(steamid_to_modify)
-            response_messages.add_message(message, False)
-            bot.tn.send_message_to_player(target_player, message, color=bot.chat_colors['warning'])
 
         return response_messages
+    else:
+        raise ValueError("action does not fully match the trigger-string")
 
 
 common.actions_list.append({
