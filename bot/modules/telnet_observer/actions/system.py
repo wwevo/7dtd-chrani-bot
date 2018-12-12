aa5f12b625540195b1f630ccefe2f0a054edef3f
Christian Voigt <wwevo@users.noreply.github.com>
2018-12-12 10:43:22 +0100

    core: accidentally removed the bc-chatprefix method *g*

--- bot/modules/telnet_observer/actions/system.py
+++ bot/modules/telnet_observer/actions/system.py
@@ -602,10 +602,66 @@ common.actions_dict["settime"] = {
 }
 
 
+def bc_chatprefix():
+    chrani_bot = __main__.chrani_bot
+    command = "bc-chatprefix"
+    if not common.actions_dict[command]["is_available"]:
+        time.sleep(1)
+        return
+
+    is_active = common.get_active_action_status('system', command)
+    if not is_active:
+        try:
+            chrani_bot.telnet_observer.tn.write("{command} \"/\"{line_end}".format(command=command, line_end=b"\r\n"))
+        except Exception as e:
+            log_message = 'trying to {command} on telnet connection failed: {error} / {error_type}'.format(command=command, error=e, error_type=type(e))
+            logger.error(log_message)
+            raise IOError(log_message)
+
+        logger.debug("starting '{command}'".format(command=command))
+        common.set_active_action_status('system', command, True)
+        thread.start_new_thread(common.actions_dict[command]["action_callback"], ())
+    else:
+        logger.debug("command '{command}' is active and waiting for a response!".format(command=command))
 
 
-"""
+def bc_chatprefix_callback_thread():
+    chrani_bot = __main__.chrani_bot
+    command = "bc-chatprefix"
+    common.active_actions_dict[command] = True
+    common.actions_dict[command]["last_executed"] = time.time()
+    poll_is_finished = False
+
+    while not poll_is_finished and not timeout_occurred(3, common.actions_dict[command]["last_executed"]):
+        logger.debug("waiting for response of '{command}'".format(command=command))
+        m = re.search(r"\*\*\* ERROR: unknown command \'{command}\'".format(command=command), chrani_bot.telnet_observer.telnet_buffer)
+        if m:
+            logger.debug("command not recognized: {command}".format(command=command))
+            common.actions_dict[command]["is_available"] = False
+            poll_is_finished = True
+            continue
+
+        match = False
+        for match in re.finditer(r"Executing command \'" + command + "\' by Telnet from (.*)", chrani_bot.telnet_observer.telnet_buffer):
+            poll_is_finished = True
+            pass
+
+        if match:
+            common.set_active_action_result('system', command, match.group(0))
+        time.sleep(0.5)
+
+    logger.debug("finished '{command}'".format(command=command))
+    common.set_active_action_status('system', command, False)
+    return
+
+
+common.actions_dict["bc-chatprefix"] = {
+    "telnet_command": "bc-chatprefix",
+    "action": bc_chatprefix,
+    "action_callback": bc_chatprefix_callback_thread,
+    "is_available": True
+}
 
 
 
-"""
+
