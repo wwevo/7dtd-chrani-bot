7c8b733e4526090891247633cb84b756def25cb3
Christian Voigt <wwevo@users.noreply.github.com>
2019-01-09 00:51:04 +0100

    core: adjusted example files

--- example-bot.sh
+++ example-bot.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-pgrep -f "chrani-bot.py example-bot" || . /opt/chrani-bot/venv_pypy/bin/activate && nohup python /opt/chrani-bot/app/chrani-bot.py example-bot --verbosity=DEBUG &
+pgrep -f "chrani-bot.py chrani-bot_example" || . /opt/chrani-bot/venv_app/bin/activate && nohup python /opt/chrani-bot/app/chrani-bot.py chrani-bot_example --verbosity=DEBUG &
