6542fa463495bf75543dd0cb3d982b4393ec9881
Christian Voigt <wwevo@users.noreply.github.com>
2019-01-09 01:57:24 +0100

    debug test

--- bot/modules/telnet_observer/actions/system.py
+++ bot/modules/telnet_observer/actions/system.py
@@ -461,6 +461,7 @@ def bc_lp_callback_thread():
         match = False
         for match in re.finditer(r"^(?P<datetime>.+?) (?P<stardate>.+?) INF Executing command \'{command} /online /1l /filter=EntityId,Name,Position,Rotation,Remote,Health,Deaths,Score,Level,SteamId,IP,Ping,Friends,OnGround\' by Telnet from (.*)\s(?P<players>\[.*\])".format(command=command), chrani_bot.telnet_observer.telnet_buffer, re.MULTILINE):
             logger.debug("************** BC-LP FOUND **********************")
+            logger.debug("************** " + chrani_bot.telnet_observer.telnet_buffer + " ****************************")
             poll_is_finished = True
             pass
 
